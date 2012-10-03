3ebf982f674d38b26b1f5c03601a337cf498553b
abe tusk <abetusk@gmail.com>
2012-10-02 20:53:17 -0400

    stupid scripts to do the conversion from mm to inches and vice versa for some grbl gcode files

---
+++ grbl_mm2inch.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import re
+import sys
+
+for line in sys.stdin:
+  li = line.rstrip()
+  l = re.compile("([xyzXYZ]\s*-?\d+\.?\d+)").split(li)
+
+  s = ''
+  for w in l:
+    if s != '':
+      s += ' '
+    if re.compile("^[xyzXYZ]").match(w):
+      m = re.match("([xyzXYZ])\s*(.*)", w)
+      s += m.group(1)
+      s += str( format( float(m.group(2)) / 25.4, 'f' ).rstrip('0') )
+    else:
+      s += w
+
+  print s
+
+
