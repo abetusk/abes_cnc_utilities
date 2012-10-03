55dbebdea17abd14b11e05c678c2a3d42a29f6b2
abe tusk <abetusk@gmail.com>
2012-10-02 21:02:03 -0400

    takes off trailing '.' for floats it converts

--- grbl_inch2mm.py
+++ grbl_inch2mm.py
@@ -14,7 +14,7 @@ for line in sys.stdin:
     if re.compile("^[xyzXYZ]").match(w):
       m = re.match("([xyzXYZ])\s*(.*)", w)
       s += m.group(1)
-      s += str( format( float(m.group(2)) * 25.4, 'f').rstrip('0') )
+      s += str( format( float(m.group(2)) * 25.4, 'f').rstrip('0').rstrip('.') )
     else:
       s += w
 
