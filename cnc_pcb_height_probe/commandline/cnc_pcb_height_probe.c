82dc080618c45e5310f8e8855761e2ff62e1dd6f
abe tusk <abetusk@gmail.com>
2012-10-17 23:53:28 -0400

    changing header

--- cnc_pcb_height_probe/commandline/cnc_pcb_height_probe.c
+++ cnc_pcb_height_probe/commandline/cnc_pcb_height_probe.c
@@ -1,11 +1,6 @@
-/* Name: hidtool.c
- * Project: hid-data example
- * Author: Christian Starkjohann
- * Creation Date: 2008-04-11
- * Tabsize: 4
- * Copyright: (c) 2008 by OBJECTIVE DEVELOPMENT Software GmbH
- * License: GNU GPL v2 (see License.txt), GNU GPL v3 or proprietary (CommercialLicense.txt)
- * This Revision: $Id$
+/* 
+ * Original project based on hid-data example by Christian Starkjohann
+ * License: GNU GPL v3 
  */
 
 #include <stdio.h>
