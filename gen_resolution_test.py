980811afdc0d9808c0f4d4a27bc81784946ba5b6
abe tusk <abetusk@gmail.com>
2012-11-18 15:53:09 -0500

    took out some cruft

--- gen_resolution_test.py
+++ gen_resolution_test.py
@@ -58,44 +58,6 @@ def draw_line( l, break_segment_length = 0.1 ):
   print "g1 x" + str(x1) + " y" + str(y1)
   print "g1 z" + str(z_up)
 
-num_height = .35
-def draw_num( num, x_start, y_start ):
-
-  # use a big trace_width and clearance to make sure it's visible
-  h_rect_x = [ 0.04, 0.12 ]
-  h_rect_y = [ 0.0 , 0.20 ]
-
-  v_rect_x = [ 0.0 , 0.20 ]
-  v_rect_y = [ 0.04, 0.12 ]
-
-  seg = [ [ h_rect_x[0], h_rect_y[0], h_rect_x[1], h_rect_y[1] ],
-          [ v_rect_x[0], v_rect_y[0], v_rect_y[1], v_rect_y[1] ],
-          [ v_rect_x[0] + .14, v_rect_y[0], v_rect_x[1] + .14, v_rect_y[1] ],
-          [ h_rect_x[0], h_rect_y[0] + .14, h_rect_x[1], h_rect_y[1] + .14 ],
-          [ v_rect_x[0], v_rect_y[0] + .18, v_rect_x[1], v_rect_y[1] + .18 ],
-          [ v_rect_x[0] + .14, v_rect_y[0] + .18, v_rect_x[1] + .14, v_rect_y[1] + .18],
-          [ h_rect_x[0], h_rect_y[0] + .28, h_rect_x[1], h_rect_y[1] + .28 ] ]
-
-  num_seg = [ [ 1, 0, 2, 5, 6, 4 ],  # 0
-              [ 2, 5],               # 1
-              [ 0, 2, 3, 4, 6 ],     # 2
-              [ 0, 2, 3, 5, 6 ],     # 3
-              [ 1, 3, 2, 5 ],        # 4
-              [ 0, 1, 3, 5, 6],      # 5
-              [ 0, 1, 3, 5, 6, 4],   # 6
-              [ 0, 2, 5 ],           # 7
-              [ 0, 1, 2, 3, 4, 6, 5], # 8
-              [ 2, 0, 1, 3, 5]      # 9
-              ]
-
-  while ( num > 0 ):
-    digit = num % 10
-    num /= 10
-
-    for pos in num_seg[digit]:
-      draw_rect( seg[ pos ] )
-
-
 def draw_num_simple( num, x_start, y_start, height, width, space, break_segment = 0.1 ):
   #   .66666.
   #   4     5
@@ -141,9 +103,6 @@ def draw_num_simple( num, x_start, y_start, height, width, space, break_segment 
 
   for c in s:
     digit = int(c)
-#  while ( num > 0 ):
-#    digit = num % 10
-#    num /= 10
 
     for pos in num_seg[digit]:
       l = seg[ pos ]
@@ -152,51 +111,6 @@ def draw_num_simple( num, x_start, y_start, height, width, space, break_segment 
     x_start += width + space
 
 
-
-def draw_test( tool_offset, trace_width, clearance, x_start, y_start, width_length, width, height_length, height, n_lines ):
-
-  dx = trace_width + clearance
-  dy = trace_width + clearance
-
-  pitch = trace_width + clearance
-
-  h_start = width - width_length
-
-  for l in range(n_lines):
-    cur_x = x_start + (pitch*float(l)) - tool_offset
-    cur_y = y_start 
-
-
-    print "g1 z" + str(z_up)
-    print "g0 x" + str(cur_x), "y" + str(cur_y)
-
-    print "g1 z" + str(z_depth)
-
-    cur_y = y_start + height_length 
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_x = x_start + width_length
-    cur_y = y_start + height + tool_offset - (pitch * float(l))
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_x = x_start + width + tool_offset
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_y -= (2.0 * tool_offset) + trace_width
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_x = x_start + width_length
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_x = x_start + (pitch * float(l)) + tool_offset + trace_width
-    cur_y = y_start + height_length
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_y = y_start - tool_offset
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
-
-    cur_x = x_start + (pitch*float(l)) - tool_offset
-    print "g1 x" + str(cur_x), "y" + str(cur_y)
 
 def draw_hor_test( pitch, x_start, y_start, length, n_lines, break_segment_length = 0.1 ):
 
@@ -345,114 +259,5 @@ for pitch_mil in pitches_mil:
     start_y = float(count_vertical * (length + 0.1) )
     start_x = 0.0
 
-sys.exit(0)
 
-
-
-
-# a angle
-#
-#          l
-#        ------
-#        |   /
-# z mil  |  /
-#        |a/
-#        |/
-#
-#  tan( a * pi / 180 ) = l / 2 -> l = 2 * tan( a * pi / 180 )
-#  
-
-angle = 20.0
-tool_offset = -z_depth * math.tan( math.pi * (angle/2.0) / 180.0 ) 
-tool_width = 2.0 * tool_offset
-
-if units == "inches":
-  pass
-elif units == "mm":
-  pass
-
-
-trace_width = 20.0 / 1000.0
-clearance = 20.0 / 1000.0
-
-trace_width_mil = int(trace_width * 1000.0)
-clearance_mil = int(clearance * 1000.0)
-
-x = 0.0
-y = 0.0
-
-# everything in mm
-#height_length = 5.0
-#height = 10.0
-#width_length = 5.0
-#width = 10.0
-
-height_length = 0.25
-height = 0.5
-width_length = 0.25
-width = 0.5
-
-pitch = trace_width + clearance
-
-print "( tool angle: ", angle, " degrees)"
-print "( tool_offset in ", units, ":", tool_offset, ")"
-print "( tool_width in ", units, ":", tool_width , ")"
-print "( trace_width in ", units, ":", trace_width, ")"
-print "( trace_width_mil :", trace_width_mil, ")"
-print "( clearance in ", units, ":", clearance , ")"
-print "( clearance_mil :", clearance_mil , ")"
-print "( height_length in ", units, ":", height_length, ")"
-print "( height in ", units, ":", height , ")"
-print "( width_length in ", units, ":", width_length, ")"
-print "( width in ", units, ":", width , ")"
-print "( x,y in ", units, ":", x, y, ")"
-
-print "g0 f100"
-print "g1 f100"
-
-#for i in range(0, 10):
-#  draw_num_simple( i, float(i)*20.0, 0.0, 10.0, 10.0  )
-
-#draw_num_simple( 9876543210, 0.0, 0.0, height_length, width_length, 2  )
-#draw_num_simple( trace_width_mil, height, width, 10.0, 10.0  )
-#draw_num_simple( clearance_mil, height, width, 10.0, 10.0  )
-shift = 1.0
-
-tw_mil = [ 5, 10, 15, 20 ]
-clr_mil = [ 5, 10 ]
-
-#tw_mil = [ 5, 10, 15, 20 ]
-#clr_mil = [ 15, 20 ]
-
-#tw_mil = [ 5, 10, 15, 20 ]
-#clr_mil = [ 8 ]
-
-#tw_mil = [ 8 ]
-#clr_mil = [ 5, 8, 10, 15, 20 ]
-
-## all
-#tw_mil = [ 5, 10, 15, 20 ]
-#clr_mil = [ 5, 8, 10, 15, 20 ]
-
-x_mod = 4
-x_counter = 0
-
-y = 0.0
-x = 0.0
-for clearance_mil in clr_mil:
-
-  for trace_width_mil in tw_mil:
-    trace_width = float(trace_width_mil) / 1000.0
-    clearance = float(clearance_mil) / 1000.0
-
-    draw_test( tool_offset, trace_width, clearance, x, y, width_length, width,height_length, height,  5 )
-    draw_num_simple( trace_width_mil , x + width_length, y + height_length/2.0, (width_length/2.0) - 0.01, (height_length/2.0) - 0.01, .1  )
-    draw_num_simple( clearance_mil, x + width_length + (width_length/2.0), y, (width_length/2.0) - 0.01, (height_length/2.0) - 0.01, .1  )
-
-    x_counter += 1
-    if ( (x_counter % x_mod) == 0):
-      y += shift
-      x = 0.0
-    else:
-      x += shift
 
